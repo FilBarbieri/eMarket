e6d25008b24ad0d339e5c7a666578d61f1a0d8ab
Fil1998 <68510194+FilBarbieri@users.noreply.github.com>
2022-07-05 13:02:07 +0200

    CodeMR

---
+++ eMarket/initial/codemr/initial/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"initial", "Number of Highly Problematic Classes":0, "Number of Entities":20, "Number of Problematic Classes":0, "Class Lines of Code":606, "Number of External Packages":19, "Number of Packages":5, "Number of External Entities":65}
---
+++ eMarket/initial/codemr/initial/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,2],[0,0,0,0,0],[0,0,0,0,0]]
+var packages = [{
+"name": " emarket", "color": " #3182bd"
+}
+,{
+"name": " emarket.registrazione", "color": " #6baed6"
+}
+,{
+"name": " emarket.gestioneOrdini", "color": " #9ecae1"
+}
+,{
+"name": " emarket.gestioneProdotto", "color": " #c6dbef"
+}
+,{
+"name": " emarket.gestioneCorriere", "color": " #e6550d"
+}
+];
